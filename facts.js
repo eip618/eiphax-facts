ffa3e003da5978a7dfabb230cd0b72923e6f1485
eip <36315290+eip618@users.noreply.github.com>
2024-11-13 15:26:53 +1100

    several major changes including random fact button
    1. hardcode index numbers to play nice with random fact button
    2. implement random fact button (holy shit!)
    3. move scripting out to facts.js
    4. minor formatting changes
    5. some features arent new, so remove 'new' label

---
+++ facts.js
@@ -0,0 +1,44 @@
+  // add ID class for anchor linking
+  const table = document.querySelector('.facts');
+
+  const tdElements = table.querySelectorAll('td');
+
+  tdElements.forEach((td, index) => {
+    td.setAttribute('id', index + 1);
+  });
+  const modifiedHTML = table.outerHTML;
+
+  table.outerHTML = modifiedHTML;
+
+  // random fact button guts
+  document.getElementById("random-fact-button").addEventListener("click", function() {
+    const factRows = document.querySelectorAll('.fact');
+
+    const randomFactRow = factRows[Math.floor(Math.random() * factRows.length)];
+
+    const factText = randomFactRow.innerHTML;
+
+    const factColorClass = randomFactRow.classList[0];
+
+    const factDisplay = document.getElementById("random-fact-display");
+    factDisplay.style.display = "block";
+    factDisplay.innerHTML = factText;
+
+    factDisplay.className = factColorClass;
+  });
+
+  // category number counters
+document.getElementById("factscount").innerHTML = document.getElementsByClassName("fact").length
+document.getElementById("naturecount").innerHTML = document.getElementsByClassName("nature").length
+document.getElementById("spacecount").innerHTML = document.getElementsByClassName("space").length
+document.getElementById("worldcount").innerHTML = document.getElementsByClassName("world").length
+document.getElementById("bodycount").innerHTML = document.getElementsByClassName("body").length
+document.getElementById("triviacount").innerHTML = document.getElementsByClassName("trivia").length
+document.getElementById("flwcount").innerHTML = document.getElementsByClassName("flw").length
+document.getElementById("peoplecount").innerHTML = document.getElementsByClassName("people").length
+document.getElementById("wincount").innerHTML = document.getElementsByClassName("win").length
+document.getElementById("jodicount").innerHTML = document.getElementsByClassName("jodi").length
+document.getElementById("pancount").innerHTML = document.getElementsByClassName("pan").length
+document.getElementById("kriegcount").innerHTML = document.getElementsByClassName("krieg").length
+document.getElementById("sammiecount").innerHTML = document.getElementsByClassName("sammie").length
+document.getElementById("recordscount").innerHTML = document.getElementsByClassName("records").length
